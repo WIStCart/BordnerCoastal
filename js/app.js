2e322a9ddde33596c109de8c5065a10dae056d0a
Codie See <csee@uwalumni.com>
2017-05-02 12:34:15 -0500

    Added several UI elements - including a demonstration of legend

--- js/app.js
+++ js/app.js
@@ -1,6 +1,7 @@
 //Global vars:
 var desktopMode = true;
-
+var level1Classes = {}
+var totalFeatures = 0
 // Load the Carto "Vis"
 window.onload = function() {
 	cartodb.createVis('map', 'https://codiesee.carto.com/api/v2/viz/4d26135a-cc79-11e6-b347-0ee66e2c9693/viz.json', {
@@ -9,33 +10,15 @@ window.onload = function() {
 		description: false,
 		search: false,
 		tiles_loader: false,
-		//center_lat: 46.38,
-		//center_lon: -91,
+		center_lat: 46.38,
+		center_lon: -91,
 		//zoom: (isMobile ? 7 : 9),
 		cartodb_logo: false
 	}).done(function(vis, layers) {
 		map = vis.getNativeMap();
-		setUpMap();
+		setUpMap(); 
 	})
-	
-	// Media query for when the app traverses the tablet/desktop threshold 
-	var jsMediaQuery = function() { 
-		if (window.matchMedia('(max-width: 768px)').matches){
-			if (desktopMode){
-				desktopMode = false;
-				console.log("~~ tablet mode engaged")
-				transformToTablet();
-			}
-		}else{
-			if (desktopMode === false){
-				desktopMode = true;
-				console.log("~~ desktop mode engaged")
-				transformToDesktop();
-			}
-		}
-	};
-	$(window).resize(jsMediaQuery);
-	jsMediaQuery(); 
+
 	console.log("~~ desktopMode = " + desktopMode)
 }
 
@@ -65,14 +48,35 @@ function setUpMap(){
 	$('[data-toggle="tooltip"]').tooltip();
 	
 	// Add WHAI tile layer 
-	map.addLayer(L.tileLayer('http://maps.sco.wisc.edu/V1/bordner/03_WHAI_Tiles/00_Demo_Kewaunee/{z}/{x}/{y}.png', {attribution: 'WHAI Finder'}))
+	// map.addLayer(L.tileLayer('http://maps.sco.wisc.edu/V1/bordner/03_WHAI_Tiles/00_Demo_Kewaunee/{z}/{x}/{y}.png', {attribution: 'WHAI Finder'}))
 	
+	// Make a demonstration legend
+	demoLegend();
+	$(window).resize(jsMediaQuery);
+	jsMediaQuery();
 	console.log("setUpMap() complete. desktopMode = " + desktopMode)
 }
 
+// Media query for when the app traverses the tablet/desktop threshold 
+var jsMediaQuery = function() { 
+	if (window.matchMedia('(max-width: 768px)').matches){
+		if (desktopMode){
+			desktopMode = false;
+			console.log("~~ tablet mode engaged")
+			transformToTablet();
+		}
+	}else{
+		if (desktopMode === false){
+			desktopMode = true;
+			console.log("~~ desktop mode engaged")
+			transformToDesktop();
+		}
+	}
+};
 // To dock/undock the table of contents from bottom 
 function toggleTOC(){
 	if ($( "#toc" ).hasClass( "toc-view-open" )){
+		$( ".level-1-label-text").removeClass( "shade-level-1-label-text" );
 		$( "#toc" ).removeClass( "toc-view-open" );
 		$( "#toc" ).addClass( "toc-view-closed" );
 		$( "#map" ).removeClass( "map-view-toc" );
@@ -82,6 +86,9 @@ function toggleTOC(){
 		$( "#tocIcon" ).removeClass( "glyphicon-chevron-down" );
 		$( "#tocIcon" ).addClass( "glyphicon-chevron-up" );
 	}else{
+		if (desktopMode){
+			$( ".level-1-label-text").addClass( "shade-level-1-label-text" );
+		}
 		$( "#toc" ).addClass( "toc-view-open" );
 		$( "#toc" ).removeClass( "toc-view-closed" );
 		$( "#map" ).addClass( "map-view-toc" );
@@ -96,11 +103,15 @@ function toggleTOC(){
 // To configure desktop view (not called upon pageload)
 function transformToDesktop(){
 	$( "#toc" ).appendTo( $( "#tocParent" ) );
+    if ($( "#toc" ).hasClass( "toc-view-open" )){
+		$( ".level-1-label-text").addClass( "shade-level-1-label-text" );
+	}	
 }
 
 // To configure tablet view (is called upon pageload)
 function transformToTablet(){
 	$( "#toc" ).appendTo( $( "#modalDialogue" ) );
+	$( ".level-1-label-text").removeClass( "shade-level-1-label-text" );
 }
 
 // Handles all click events from lower lefthand corner
@@ -139,3 +150,27 @@ var tabletCustomControl = L.Control.extend({
 		return container;
 	}
 })
+
+function demoLegend(){
+	var highestValue = 0;
+	for (var key in tempClasses) {
+		var value = tempClasses[key];
+		if (!level1Classes.hasOwnProperty(value.level1)) {
+			if (value.level1frq > highestValue){ highestValue = value.level1frq }
+			totalFeatures = totalFeatures + value.level1frq 
+			level1Classes[value.level1] = {"level1": value.level1 , "level1frq": value.level1frq , "hex1": value.hex1}
+		}
+	} 
+	// console.log(_.size(level1Classes)) playing with http://underscorejs.org/
+	level1Classes = _.indexBy(level1Classes, 'level1frq') // playing with http://underscorejs.org/
+	var countKey = 0;
+	for (var key in level1Classes) {
+		var value = level1Classes[key];
+		//featurePct = (value.level1frq / totalFeatures) * 100
+		featurePct = (value.level1frq / highestValue) * 100
+		//if (featurePct < 2){ featurePct = 2 }
+		$("#legendHolder").append('<div class="histogram-div"; style="height:' + String(featurePct) + '%; width:10%; left:' + (countKey * 10) + '%; background-color:' + value.hex1 + ';" >'
+			+ '<div style="background-color:' + value.hex1 + ';" class="level-1-label-text rotate-text shade-level-1-label-text transition-class">' + level1Classes[key].level1 + '</div></div>')
+		countKey++; 
+	}
+}
