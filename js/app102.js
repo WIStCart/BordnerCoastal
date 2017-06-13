2dd01ac6f2c7e64d520f29b775dcb1183a7d3e39
Chris Scheele <scheele.cjs@gmail.com>
2017-06-13 15:39:36 -0500

    fixed the plss labels

--- js/app102.js
+++ js/app102.js
@@ -286,11 +286,12 @@ window.onload = function() {
 	  "line-opacity: 0.5;" +
 	  "line-comp-op: soft-light;" +
 	  "[zoom > 10]{" +
-		"text-name: [twp];" +
+		"text-name: 'T'+ [twp] + 'N R'+[rng]+[dirchar];" +
 		"text-face-name: 'Open Sans Regular';" +
 	    "text-size: 13;"+
 	    "text-fill: #fff;"+
 	    "text-halo-fill: #000000;"+
+	    "text-halo-radius: 1;"+
 	  "}"+
 	"}"
 	cartodb.createLayer(map, {
